281aa3596f4fbc36cfc01a848819813b5f1bccc8
Nouman Nadeem <160357491+NoumanNadeem123@users.noreply.github.com>
2024-03-11 00:53:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Bill Gates Said, 'Don't Compare yourself with anyone in this world.. if you do so,you are insulting yourself.
+var qoute = "Bill Gates";
+var auother = "Don't Compare yourself with anyone in this world.. if you do so,you are insulting yourself.";
+console.log("".concat(qoute, " said, ").concat(auother));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//Bill Gates Said, 'Don't Compare yourself with anyone in this world.. if you do so,you are insulting yourself.
+
+let qoute: String = "Bill Gates"
+let auother: string= "Don't Compare yourself with anyone in this world.. if you do so,you are insulting yourself."
+console.log(`${qoute} said, ${auother}`);
